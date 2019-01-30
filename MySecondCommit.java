c578ec4c7db3c10a6e2eead9b97e086d919163e9
swathi Marri <swathimarri@swathis-MacBook-Pro.local>
2019-01-29 21:29:13 -0600

    My Secont commit for today

---
+++ MySecondCommit.java
@@ -0,0 +1,22 @@
+
+public class MySecondCommit {
+	
+	public static void main(String[] args) {
+
+		/*
+		 * Only one of this can be used
+		 */
+
+		// Inceremennt
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i  = " + i);
+
+			// Decerment
+			for (int j = 10; j > 0; j--) {
+				System.out.println("j  = " + j);
+			}
+		}
+
+	}
+
+}
